57b65c7ef70fa26b4ce3535521eb6f560560323b
abdulrahman aidarous <abdulrahmanaid.21@gmail.com>
2025-02-05 12:01:10 +0300

    Solution for LAB_CONDITIONALS

--- bonus.py
+++ bonus.py
@@ -2,8 +2,8 @@ user_wieght:float=float(input("enter your weight in kg:"))
 user_height:float=float(input("enter your height in cm:"))/100
 bmi:float=user_wieght/user_height**2
 print(f"Your BMI is: = {round(bmi,2)}")
-suggested_weight_range_from=18.5*user_height**2
-suggested_weight_range_to=24.9*user_height**2
+suggested_weight_range_from:float=18.5*user_height**2
+suggested_weight_range_to:float=24.9*user_height**2
 
 if bmi >25:
     print("You are overwieght you need to work out more and watch your diet")
